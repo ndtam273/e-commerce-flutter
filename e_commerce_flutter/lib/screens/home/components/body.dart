c7500951ad1a60f8721a3504cb25be9ff9871189
tam nguyen <ndtam.273@gmail.com>
2020-12-28 17:58:45 +0700

    add categories 1

--- e_commerce_flutter/lib/screens/home/components/body.dart
+++ e_commerce_flutter/lib/screens/home/components/body.dart
@@ -15,9 +15,27 @@ class Body extends StatelessWidget {
             height: SZ.getProportionateScreenWidth(20),
           ),
           HomeHeader(),
+          SizedBox(
+            height: SZ.getProportionateScreenWidth(20),
+          ),
           DiscountBanner(),
+          Categories()
         ],
       ),
     ));
+  }
+}
+
+class Categories extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, dynamic>> categories = [
+      {"icon": "assets/icons/Flash Icon.svg", "text": "Flash Deal"},
+      {"icon": "assets/icons/Bill Icon.svg", "text": "Bill"},
+      {"icon": "assets/icons/Game Icon.svg", "text": "Game"},
+      {"icon": "assets/icons/Gift Icon.svg", "text": "Daily Gift"},
+      {"icon": "assets/icons/Discover.svg", "text": "More"}
+    ];
+    return Container();
   }
 }
